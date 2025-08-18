9f41d6fa33714e05f88a26bae61838bc05fbc71c
kameshravada <ravadavenkatakamesh123@gmail.com>
2025-08-18 16:19:59 +0530

    s

--- src/Backend/AppConstants.jsx
+++ src/Backend/AppConstants.jsx
@@ -3,6 +3,6 @@
 // };
 
 export const AppConstants = {
-  BACKEND_URL:
-    import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api/v1.0",
+  //BACKEND_URL: import.meta.env.VITE_BACKEND_URL ,
+  BACKEND_URL: "http://localhost:8080/api/v1.0",
 };
